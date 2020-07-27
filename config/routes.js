1ce0b88c5c3e51a2b926503120716597c6d69d54
Rhafael Santos <rhafa99ss@gmail.com>
2020-07-27 17:32:25 -0300

    A attempted of change in backend

--- config/routes.js
+++ config/routes.js
@@ -3,16 +3,15 @@ module.exports = app => {
   app.post('/signin', app.api.auth.signin)
 
   app.route('/tasks')
-    .all(app.config.passport.authenticate())
-    .get(app.api.task.getTasks)
-    .post(app.api.task.save)
-
+      .all(app.config.passport.authenticate())
+      .get(app.api.task.getTasks)
+      .post(app.api.task.save)
 
   app.route('/tasks/:id')
-    .all(app.config.passport.authenticate())
-    .delete(app.api.task.remove)
+      .all(app.config.passport.authenticate())
+      .delete(app.api.task.remove)
 
   app.route('/tasks/:id/toggle')
-    .all(app.config.passport.authenticate())
-    .put(app.api.task.toggleTask)
+      .all(app.config.passport.authenticate())
+      .put(app.api.task.toggleTask)
 }
